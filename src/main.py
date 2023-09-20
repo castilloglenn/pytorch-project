43cc501228adffa7ace4b2ce9533d4cdc24cc10d
Allen Glenn Castillo <55197203+castilloglenn@users.noreply.github.com>
2023-09-20 12:48:01 +0800

    feat: fashion mnist data

--- src/main.py
+++ src/main.py
@@ -1,8 +1,35 @@
+import torch
 from absl import flags
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
 
 FLAGS = flags.FLAGS
 
 
 class Main:
     def __init__(self) -> None:
-        print(FLAGS.model.test)
+        self.download_fashion_mnist()
+        self.start()
+
+    def download_fashion_mnist(self):
+        # Download training data from open datasets.
+        self.training_data = datasets.FashionMNIST(
+            root="data",
+            train=True,
+            download=True,
+            transform=ToTensor(),
+        )
+
+        # Download test data from open datasets.
+        self.test_data = datasets.FashionMNIST(
+            root="data",
+            train=False,
+            download=True,
+            transform=ToTensor(),
+        )
+
+    def start(self):
+        print(self.training_data)
+        print(self.test_data)
